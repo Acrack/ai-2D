4f9f2b11f86265ee8c3ff3d5f63a1d5a2cbd2698
Yehya LATTI <yehya.latti@gmail.com>
2019-09-04 21:10:17 +0200

    vector

--- ball.js
+++ ball.js
@@ -1,26 +1,35 @@
 class Ball {
-  constructor() {
-    this.r = 40;
-
-    this.loc = createVector(width/2, height/2);
-    this.velocity = createVector(5, 5);
+  constructor(x, y) {
+    this.radius = 40;
+    this.position = createVector(x, y);
+    this.velocity = createVector(0, 0);
+    this.acceleration = createVector(0, 0);
   }
 
   move() {
-    this.loc.add(this.velocity);
+    this.mouse = createVector(mouseX, mouseY);
+    this.mouse.sub(this.position);
+    this.mouse.setMag(0.1);
+
+    this.acceleration = this.mouse;
+    this.velocity.add(this.acceleration);
+    this.position.add(this.velocity);
+
+    stroke(255);
+    line(this.position.x, this.position.y, mouseX, mouseY);
   }
 
   bounce() {
-    if ((this.loc.x + 20 > width) || this.loc.x < 20) {
+    if ((this.position.x + 20 > width) || this.position.x < 20) {
       this.velocity.x = this.velocity.x * -1;
     }
 
-    if ((this.loc.y + 20 > height) || this.loc.y < 20) {
+    if ((this.position.y + 20 > height) || this.position.y < 20) {
       this.velocity.y = this.velocity.y * -1;
     }
   }
 
   display() {
-    ellipse(this.loc.x, this.loc.y, this.r);
+    ellipse(this.position.x, this.position.y, this.radius);
   }
 }
--- sketch.js
+++ sketch.js
@@ -1,13 +1,21 @@
 function setup() {
   createCanvas(720, 400);
 
-  ball = new Ball();
+  elements = 5;
+  population = [];
+
+  for (let i = 0; i < elements; i++) {
+    population.push(new Ball(random(10, 50), random(50, 200)));
+  }
 }
 
 function draw() {
   background(0);
+  stroke(255);
 
-  ball.move();
-  ball.bounce();
-  ball.display();
+  for (let i = 0; i < population.length; i++) {
+    population[i].move();
+    population[i].bounce();
+    population[i].display();
+  }
 }
