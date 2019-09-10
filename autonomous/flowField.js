79d49a057c7ce03c9def6155291c96c4b3916ede
Yehya LATTI <yehya.latti@gmail.com>
2019-09-10 20:09:34 +0200

    flow field

--- autonomous/flowField.js
+++ autonomous/flowField.js
@@ -3,23 +3,54 @@ class FlowField {
     this.resolution = resolution;
     this.cols = width / this.resolution;
     this.rows = height / this.resolution;
-    this.field = createVector([cols], [rows]);
+    this.field = [];
+
+    this.init();
   }
 
   init() {
     let xoff = 0;
-    for (i = 0; i < this.cols; i++) {
+
+    for (let i = 0; i < this.cols; i++) {
       let yoff = 0;
-      for (j = 0; j < this.rows; j++) {
-        theta = 0;
-        field[i][j] = 0;
+
+      this.field[i] = [];
+
+      for (let j = 0; j < this.rows; j++) {
+        let theta = map(noise(xoff,yoff), 0, 1, 0, TWO_PI);
+
+        this.field[i][j] = createVector(cos(theta), sin(theta));
+
         yoff += 0.1;
-      } 
+      }
+
       xoff += 0.1;
     }
   }
 
-  display() {
+  drawVector(vector, x, y, scale) {
+    push();
 
+    let arrowsize = 2;
+
+    translate(x, y);
+
+    rotate(vector.heading());
+
+    let length = vector.mag() * scale;
+
+    line(0, 0, length, 0);
+    //line(length, 0, length - arrowsize, arrowsize / 2);
+    //line(length, 0, length - arrowsize, -arrowsize / 2);
+
+    pop();
+  }
+
+  display() {
+    for (let i = 0; i < this.cols; i++) {
+      for (let j = 0; j < this.rows; j++) {
+        this.drawVector(this.field[i][j], i * this.resolution, j * this.resolution, this.resolution - 2);
+      }
+    }
   }
 }
--- autonomous/sketch.js
+++ autonomous/sketch.js
@@ -1,12 +1,15 @@
 function setup() {
   createCanvas(720, 400);
 
+  flowField = new FlowField(10);
   vehiculeA = new Vehicule(20, 20);
   vehiculeB = new Vehicule(width/2, height/2);
 }
 
 function draw() {
   background(200);
+
+  flowField.display();
 
   vehiculeA.seek(vehiculeB.position);
 
