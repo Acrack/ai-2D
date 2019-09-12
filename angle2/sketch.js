9e9c4be55bd2a988463cd31c623dad6f08f86e85
Yehya LATTI <yehya.latti@gmail.com>
2019-09-12 18:52:54 +0200

    sin

--- angle2/sketch.js
+++ angle2/sketch.js
@@ -1,52 +1,61 @@
-function setup() {
-  createCanvas(720, 400);
+let angle = 0;
+let aVel = 0.0; 
+let aAcc = 1/30;
+const history = [];
 
-  this.angle = 0;
-  this.aVel = 0.0; 
-  this.aAcc = 0.001;
+function setup() {
+  createCanvas(800, 450);
 }
 
 function draw() {
   background(200);
 
-  this.aVel += this.aAcc;
-  this.angle += this.aVel;
-  this.aVel = constrain(aVel, 0, 0.5);
+  circle(width / 2, height / 2, 200);
+
+  aVel += aAcc;
+  angle += aVel;
+  aVel = constrain(aVel, 0, 0.002);
 
   push();
     translate(width / 2, height / 2);
-    let vector = createVector(0, -100);
+    let vector = createVector(100, 0);
     vector.setMag(100);
-
-    drawVector(vector);
-    drawArrows(vector);
+    line(0, 0, vector.x, vector.y);
   pop();
 
   push();
     translate(width / 2, height / 2);
-    vector.rotate(this.angle);
+    vector.rotate(-angle);
+    vector.setMag(100);
+    history.push(createVector(150, vector.y));
 
-    if (angle > 120) {
-      this.angle = this.angle * -1;
-    }
-
-    vector.setMag(angle + 10);
-
-    drawVector(vector);
-    drawArrows(vector);
+    line(0, 0, vector.x, vector.y);
+    line(vector.x, vector.y, 150, vector.y);
+    circle(150, vector.y, 5);
   pop();
-}
 
-function drawVector(vector) {
-  line(0, 0, vector.x, vector.y);
+  strokeWeight(2);
+
+  translate(width / 2, height / 2);
+
+  noFill();
+  beginShape();
+  for (let i = history.length; i > 0; i--) {
+    let currentVector = history[i];
+    
+    if (typeof currentVector !== 'undefined') {
+      vertex(currentVector.x ++, currentVector.y);
+    }
+  }
+  endShape();
 }
 
 function drawArrows(vector) {
   translate(vector.x, vector.y);
-  let arrow = p5.Vector.fromAngle(vector.heading());
-  arrow.setMag(5);
-  rotate(radians(135));
+  let arrow = p5.Vector.fromAngle(vector.heading() - PI, 5);
+  rotate(PI / 4);
   line(0, 0, arrow.x, arrow.y);
-  rotate(radians(90));
+  rotate(-PI / 2);
   line(0, 0, arrow.x, arrow.y);
 }
+ 
