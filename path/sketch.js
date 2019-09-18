aaf7f59bd0a7168c185fe39da56fb50288495fb7
Yehya LATTI <yehya.latti@gmail.com>
2019-09-18 19:51:38 +0200

    Fix

--- path/sketch.js
+++ path/sketch.js
@@ -1,8 +1,3 @@
-let angle = 0;
-let aVel = 0.0; 
-let aAcc = 1/10;
-const history = [];
-
 function setup() {
   createCanvas(800, 450);
 }
@@ -12,17 +7,15 @@ function draw() {
 
   push();
     translate(width / 2, height / 2);
-    let path = createVector(200, 150);
+
+    let path = createVector(200, 0);
     line(0, 0, path.x, path.y);
-  pop();
 
-  let currentDirection = createVector(mouseX, mouseY);
-  line(width / 2, height / 2, mouseX, mouseY);
+    let currentDirection = createVector(mouseX - width / 2, mouseY - height / 2);
+    line(0, 0, currentDirection.x, currentDirection.y);
 
-  push();
-    translate(width / 2, height / 2);
     let projection = resolveProjection(currentDirection, path);
-    line(mouseX - width / 2, mouseY - height / 2, projection.x - width / 2, projection.y - height / 2);
+    line(currentDirection.x, currentDirection.y, projection.x, projection.y);
   pop();
 }
 
@@ -30,14 +23,5 @@ function resolveProjection(currentDirection, path) {
   let normalizedPath = path.normalize();
   
   return normalizedPath.mult(currentDirection.dot(path));
-}
-
-function drawArrows(vector) {
-  translate(vector.x, vector.y);
-  let arrow = p5.Vector.fromAngle(vector.heading() - PI, 5);
-  rotate(PI / 4);
-  line(0, 0, arrow.x, arrow.y);
-  rotate(-PI / 2);
-  line(0, 0, arrow.x, arrow.y);
 }
  
