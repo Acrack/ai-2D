c2f653c01cdef52026fc411b88ff4e37f2f8ddf8
Yehya LATTI <yehya.latti@gmail.com>
2019-09-25 10:45:11 +0200

    linear

---
+++ linear/sketch.js
@@ -0,0 +1,26 @@
+let data;
+let m;
+let b;
+
+function setup() {
+  createCanvas(720, 400);
+  
+  data = [];
+  m = 0;
+  b = 0;
+}
+
+function draw() {
+  background(0);
+
+}
+
+function linearRegression() {
+
+}
+
+drawLine() {
+  let x1 = 0;
+  let y = m * x1 + b;
+  let x2 = 1;
+}
